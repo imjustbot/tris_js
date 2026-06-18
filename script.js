910fabbb8fccb67af19423e2f09118b07102438d
imjustbot <nico7tolu@gmail.com>
2026-06-18 20:38:22 +0200

    update js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const caselle = document.querySelectorAll('.casella');
 
-const combinazioniVincenti = [,
- ,
- ,
- ,
- ,
- ,
- ,
-    [2, 4, 6]
+const combinazioniVincenti = [
+  [ 0, 1, 2 ],
+  [ 3, 4, 5 ],
+  [ 6, 7, 8 ],
+  [ 0, 3, 6 ],
+  [ 1, 4, 7 ],
+  [ 2, 5, 8 ],
+  [ 0, 4, 8 ],
+  [ 2, 4, 6 ]
 ];
 
 caselle.forEach(casella => {
